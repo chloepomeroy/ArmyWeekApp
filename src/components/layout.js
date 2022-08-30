3c268f95dd33fd177591c513327842ba60119e54
chloepomeroy <pomeroyc@outlook.com>
2022-08-30 16:04:30 -0400

    add back to top in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,31 +1,17 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
 
 import * as React from "react"
 import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
+import BackToTop from "./BackToTop/backToTop"
 
 import "./layout.css"
 import ResponsiveAppBar from "./Navbar/navbar"
 
 const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
 
   return (
     <>
     <ResponsiveAppBar />
+    <BackToTop/>
       {/* <div
         style={{
           margin: `0 auto`,
