79838a427425a8c854609b15d67cdd84460fc10d
chloepomeroy <pomeroyc@outlook.com>
2022-09-12 14:14:19 -0400

    updated french agenda

--- contract/target/wasm32-unknown-unknown/debug/greeter.d
+++ contract/target/wasm32-unknown-unknown/debug/greeter.d
@@ -1,1 +1,1 @@
-/home/vitalpointai/projects/space-gems/contract/target/wasm32-unknown-unknown/debug/greeter.wasm: /home/vitalpointai/projects/space-gems/contract/src/lib.rs
+/home/lo/ArmyWeekApp-1/contract/target/wasm32-unknown-unknown/debug/greeter.wasm: /home/lo/ArmyWeekApp-1/contract/src/lib.rs
