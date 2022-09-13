7b58e70d7ec9ac155091e2c3954f741d558374e0
chloepomeroy <pomeroyc@outlook.com>
2022-09-13 02:23:36 -0400

    .

--- src/components/mainPages/dashboards.js
+++ src/components/mainPages/dashboards.js
@@ -10,172 +10,53 @@ import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 
-// Dashboard Images
-import awRegistrations from '../../img/armyweekregss.jpg'
-import cams from '../../img/camsss.jpg'
-import army101 from '../../img/army101ss.jpg'
-import armyOrg from '../../img/orgstructss.jpg'
-import bios from '../../img/CAGenOffss.jpg'
-import cearp from '../../img/cearp.jpg'
-import g1 from '../../img/g1.jpg'
+const baseImgUrl = 'https://caarmydata.blob.core.windows.net/images/'
 
 export default function Dashboards() {
 
     const { t, i18n } = useTranslation();
 
+    const CardItem = props => {
+       
+        return (
+            <Card sx={{ maxWidth: '98%' }} key={props.db.id}>
+                <CardContent sx={{textAlign: 'left'}}>
+                    <img src={props.imgSrc} alt={props.db.file_title} style={{maxWidth: '98%'}} />
+                    <Typography variant="h5">
+                        {props.db.title}
+                    </Typography>
+                    <Typography variant="body1">
+                        {props.db.description}
+                    </Typography>
+                </CardContent>
+                <CardActionArea>
+                    <CardActions>
+                        <Button size="small" href={props.db.embed_url}>{t("dashboards_view")}</Button>
+                    </CardActions>
+                </CardActionArea>
+            </Card>
+        )
+    }
+
+    const CardList = () => {
+        return (
+            <div>
+            {t("dashboard_list", { returnObjects: true }).map((db, index) => {
+            let imgSrc = baseImgUrl+db.img 
+            return <CardItem db={db} imgSrc={imgSrc}/>
+            })}
+            </div>
+        )
+    }
+
+
     return (
         <Box sx={{ width: '100%', height: '100vh', bgcolor: 'white', marginTop: '50px', paddingTop: '30px'  }}>
         <Grid container alignItems="center" justifyContent="center">
             <Stack direction="column"  justifyContent="center" alignItems="center" spacing={3} style={{maxWidth: '90%', marginBottom: '76px'}}>
-                
-                <Card sx={{ maxWidth: 'auto' }}>
-                    <CardContent align="left">
-                        <img src={bios} alt="1st floor" style={{maxWidth: '98%'}} />
-                        <Typography variant="h5">
-                            {t("dashboard_list", { returnObjects: true })[4].title}
-                        </Typography>
-                        <Typography variant="body1">
-                            {t("dashboard_list", { returnObjects: true })[4].description}
-                        </Typography>
-                    </CardContent>
-                    <CardActionArea>
-                        <CardActions>
-                            <a href={t("dashboard_list", { returnObjects: true })[4].embed_url}>
-                                <Typography variant="overline">
-                                    {t("dashboards_view")}
-                                </Typography>
-                            </a>
-                        </CardActions>
-                    </CardActionArea>
-                </Card>
-
-                <Card sx={{ maxWidth: 'auto' }}>
-                    <CardContent align="left">
-                        <img src={cams} alt="1st floor" style={{maxWidth: '98%'}} />
-                        <Typography variant="h5">
-                            {t("dashboard_list", { returnObjects: true })[1].title}
-                        </Typography>
-                        <Typography variant="body1">
-                            {t("dashboard_list", { returnObjects: true })[1].description}
-                        </Typography>
-                    </CardContent>
-                    <CardActionArea>
-                        <CardActions>
-                            <a href={t("dashboard_list", { returnObjects: true })[1].embed_url}>
-                                <Typography variant="overline">
-                                    {t("dashboards_view")}
-                                </Typography>
-                            </a>
-                        </CardActions>
-                    </CardActionArea>
-                </Card>
-
-                <Card sx={{ maxWidth: 'auto' }}>
-                    <CardContent align="left">
-                        <img src={army101} alt="1st floor" style={{maxWidth: '98%'}} />
-                        <Typography variant="h5">
-                            {t("dashboard_list", { returnObjects: true })[2].title}
-                        </Typography>
-                        <Typography variant="body1">
-                            {t("dashboard_list", { returnObjects: true })[2].description}
-                        </Typography>
-                    </CardContent>
-                    <CardActionArea>
-                        <CardActions>
-                            <a href={t("dashboard_list", { returnObjects: true })[2].embed_url}>
-                                <Typography variant="overline">
-                                    {t("dashboards_view")}
-                                </Typography>
-                            </a>
-                        </CardActions>
-                    </CardActionArea>
-                </Card>
-
-                <Card sx={{ maxWidth: 'auto' }}>
-                    <CardContent align="left">
-                        <img src={awRegistrations} alt="1st floor" style={{maxWidth: '98%'}} />
-                        <Typography variant="h5">
-                            {t("dashboard_list", { returnObjects: true })[0].title}
-                        </Typography>
-                        <Typography variant="body1">
-                            {t("dashboard_list", { returnObjects: true })[0].description}
-                        </Typography>
-                    </CardContent>
-                    <CardActionArea>
-                        <CardActions>
-                            <a href={t("dashboard_list", { returnObjects: true })[0].embed_url}>
-                                <Typography variant="overline">
-                                    {t("dashboards_view")}
-                                </Typography>
-                            </a>
-                        </CardActions>
-                    </CardActionArea>
-                </Card>
-
-                <Card sx={{ maxWidth: 'auto' }}>
-                    <CardContent align="left">
-                        <img src={armyOrg} alt="1st floor" style={{maxWidth: '98%'}} />
-                        <Typography variant="h5">
-                            {t("dashboard_list", { returnObjects: true })[3].title}
-                        </Typography>
-                        <Typography variant="body1">
-                        {t("dashboard_list", { returnObjects: true })[3].description}
-                        </Typography>
-                    </CardContent>
-                    <CardActionArea>
-                        <CardActions>
-                            <a href={t("dashboard_list", { returnObjects: true })[4].embed_url}>
-                                <Typography variant="overline">
-                                    {t("dashboards_view")}
-                                </Typography>
-                            </a>
-                        </CardActions>
-                    </CardActionArea>
-                </Card>
-
-                <Card sx={{ maxWidth: 'auto' }}>
-                    <CardContent align="left">
-                        <img src={g1} alt="1st floor" style={{maxWidth: '98%'}} />
-                        <Typography variant="h5">
-                            {t("dashboard_list", { returnObjects: true })[5].title}
-                        </Typography>
-                        <Typography variant="body1">
-                            {t("dashboard_list", { returnObjects: true })[5].description}
-                        </Typography>
-                    </CardContent>
-                    <CardActionArea>
-                        <CardActions>
-                            <a href={t("dashboard_list", { returnObjects: true })[5].embed_url}>
-                                <Typography variant="overline">
-                                    {t("dashboards_view")}
-                                </Typography>
-                            </a>
-                        </CardActions>
-                    </CardActionArea>
-                </Card>
-
-                <Card sx={{ maxWidth: 'auto' }}>
-                    <CardContent align="left">
-                        <img src={cearp} alt="1st floor" style={{maxWidth: '98%'}} />
-                        <Typography variant="h5">
-                            {t("dashboard_list", { returnObjects: true })[6].title}
-                        </Typography>
-                        <Typography variant="body1">
-                            {t("dashboard_list", { returnObjects: true })[6].description}
-                        </Typography>
-                    </CardContent>
-                    <CardActionArea>
-                        <CardActions>
-                            <a href={t("dashboard_list", { returnObjects: true })[6].embed_url}>
-                                <Typography variant="overline">
-                                    {t("dashboards_view")}
-                                </Typography>
-                            </a>
-                        </CardActions>
-                    </CardActionArea>
-                </Card>
-
+                <CardList />
             </Stack>
         </Grid>
         </Box>
